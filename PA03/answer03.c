3fc5755d53de1e06b0891f5462bd6f1d7202f599
Mochen Linghu <mlinghu@ecegrid-thin3.ecn.purdue.edu>
2014-09-16 11:30:08 -0400

    All pass

--- PA03/answer03.c
+++ PA03/answer03.c
@@ -46,21 +46,23 @@ char ** explode(const char * str, const char * delims, int * arrlen)
         (*arrlen)++;
       }
   }
-  ans = (char **) malloc( 2*(*arrlen) * sizeof(char*) );
+  ans = (char **) malloc( (2*(*arrlen) + 1) * sizeof(char*) );
   int pre=0 , cnt=0;
   for (i = 0; i < len; ++i)
   {
     if (strchr(delims,str[i])!=NULL)
       {
-        ans[cnt]=(char *)malloc((2*(i-pre))*sizeof(char));
-        memcpy(ans[cnt],str+pre,(i-pre)*sizeof(char));
+        int slen = (2*(i-pre) + 1) * sizeof(char);
+        ans[cnt]=(char *)malloc(slen);
+        memcpy(ans[cnt],str+pre,(i-pre + 1)*sizeof(char));
         ans[cnt++][i-pre]='\0';
         pre=i+1;
       }
   }
-  ans[cnt]=(char *)malloc((2*(len-pre))*sizeof(char));
-  memcpy(ans[cnt],str+pre,(len-pre)*sizeof(char));
+  ans[cnt]=(char *)malloc((2*(len-pre) + 1)*sizeof(char));
+  memcpy(ans[cnt],str+pre,(len-pre + 1)*sizeof(char));
   ans[cnt++][len-pre]='\0';
+  *arrlen = cnt;
   return ans;
 }
 
@@ -69,44 +71,44 @@ char * implode(char ** strArr, int len, const char * glue)
   int ind_str;
   int n = 0;
   char * s = NULL;
+  if (*strArr == NULL)
+  {
+    return s;
+  }
+  if (len <= 1)
+  {
+    strcat_ex(&s, &len, strArr[0]);
+    return s;
+  }
   for (ind_str = 0; ind_str < len; ind_str++)
-    {
-      strArr[ind_str][strlen(strArr[ind_str])] = *glue;
+  {
       s = strcat_ex(&s, &n, strArr[ind_str]);
-      
-    }
+      if (ind_str == len -1)
+      {
+        break;
+      }
+      s = strcat_ex(&s, &n, glue);
+  }
   
   return s;
+}
 
-}
-/*
-void sortStringArray(char ** arrString, int len);
+
+
+int compStr(const void *a, const void  *b)
 {
-  int index_arr;
-  int index_s;
-  for (index_arr = 0; index_arr < (len - 1); index_arr++)
-    {
-      index_s = 0;
-      while (arrString[index_arr][index_s] != '\0')
-      {
-        if (arrString[index_arr][index_s] == arrString[index_arr + 1][index_s])
-          {
-            index_s++;
-          }
-        if (arrString[index_arr][index_s] > arrString[index_arr + 1][index_s])
-          {
-            swapString(&arrString[index_arr], &arrString[index_arr + 1]);
-            break;
-          }
-        else
-          {
-            break;
-          }
-      }
-    }
-    return 0;
+  int ans = strcmp(*(char **)a,*(char **)b);
+  return(ans);
 }
-*/
+void sortStringArray(char ** arrString, int len)
+{
+  qsort(arrString, len, sizeof(char *), compStr);
+  return;
+}
+
+
+
+
 int compAsc(const void * a, const void * b)
 {
   return(*(char *)a -*(char *)b);
@@ -117,7 +119,21 @@ void sortStringCharacters(char * str)
   int n = strlen(str);
   
   qsort(str, n, sizeof(str[0]), compAsc);
+  return;
 }
 
 
-
+void destroyStringArray(char ** strArr, int len)
+{  
+  int index_str;
+  if (strArr == NULL)
+  {
+    return;
+  }
+  for (index_str = 0; index_str < len; index_str++)
+  {
+    free(strArr[index_str]);
+  }
+  free(strArr);
+return;
+}
